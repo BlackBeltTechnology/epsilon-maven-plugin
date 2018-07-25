a71270b9ab4dc642f981526191fee5572346675e
Robert Csakany <csakany.robert@gmail.com>
2018-07-25 02:06:58 +0200

    Moved to different repository

--- epsilon-utils/src/main/java/hu/blackbelt/judo/generator/utils/execution/ArtifactResolver.java
+++
@@ -1,7 +0,0 @@
-package hu.blackbelt.judo.generator.utils.execution;
-
-import org.eclipse.emf.common.util.URI;
-
-public interface ArtifactResolver {
-    URI getArtifactAsEclipseURI(String url);
-}
