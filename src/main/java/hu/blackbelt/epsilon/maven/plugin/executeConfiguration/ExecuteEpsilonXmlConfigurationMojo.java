f6487dac1a8ee518aac64de99e1f68ef0077160b
Jozsef Borcsok <jozsef.borcsok@blackbelt.hu>
2018-09-27 10:33:52 +0200

    #2 support configuration artifacts as fileset (similar to includes Maven parameter supporting wildcards used by several plugins)

--- src/main/java/hu/blackbelt/epsilon/maven/plugin/executeConfiguration/ExecuteEpsilonXmlConfigurationMojo.java
+++ src/main/java/hu/blackbelt/epsilon/maven/plugin/executeConfiguration/ExecuteEpsilonXmlConfigurationMojo.java
@@ -14,6 +14,10 @@ import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.codehaus.plexus.components.io.fileselectors.FileSelector;
+import org.codehaus.plexus.components.io.fileselectors.IncludeExcludeFileSelector;
+import org.codehaus.plexus.components.io.resources.PlexusIoFileResourceCollection;
+import org.codehaus.plexus.components.io.resources.PlexusIoResource;
 import org.eclipse.aether.RepositorySystem;
 import org.eclipse.aether.RepositorySystemSession;
 import org.eclipse.aether.repository.RemoteRepository;
@@ -21,10 +25,10 @@ import uk.org.lidalia.sysoutslf4j.context.SysOutOverSLF4J;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
-import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
-import java.util.List;
+import java.io.IOException;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Mojo(name = "executeConfiguration", defaultPhase = LifecyclePhase.GENERATE_RESOURCES)
@@ -39,8 +43,11 @@ public class ExecuteEpsilonXmlConfigurationMojo extends AbstractMojo {
     @Parameter(defaultValue = "${project.remoteProjectRepositories}", readonly = true, required = true)
     public List<RemoteRepository> repositories;
 
-    @Parameter(name = "configurationArtifact", readonly = true, required = true)
-    public String configurationArtifact;
+    /**
+     * List of configuration artifacts. Item could be Maven artifacts (mvn:*) or ant filesets (supporting wildcards).
+     */
+    @Parameter(name = "configurationArtifacts", readonly = true, required = true)
+    public String[] configurationArtifacts;
 
     @Parameter(name = "sourceDirectory", defaultValue = "${basedir}", required = true, readonly = true)
     public File sourceDirectory;
@@ -52,84 +59,118 @@ public class ExecuteEpsilonXmlConfigurationMojo extends AbstractMojo {
     }
 
     synchronized public void execute() throws MojoExecutionException, MojoFailureException {
-        final File configurationArtifactFile = MavenArtifactResolver.builder()
-                .repoSession(repoSession)
-                .repositories(repositories)
-                .repoSystem(repoSystem)
-                .sourceDirectory(sourceDirectory)
-                .log(log)
-                .build()
-                .getArtifact(configurationArtifact);
+        final List<File> configurationArtifactFiles = new LinkedList<>();
+        final List<String> includes = new LinkedList<>();
 
-        if (!configurationArtifactFile.exists()) {
-            throw new MojoFailureException("Configuration artifact file not found: " + configurationArtifactFile);
+        for (final String configurationArtifact : configurationArtifacts) {
+            if (configurationArtifact.startsWith("mvn:")) {
+                configurationArtifactFiles.add(MavenArtifactResolver.builder()
+                        .repoSession(repoSession)
+                        .repositories(repositories)
+                        .repoSystem(repoSystem)
+                        .sourceDirectory(sourceDirectory)
+                        .log(log)
+                        .build()
+                        .getArtifact(configurationArtifact));
+            } else {
+                includes.add(configurationArtifact);
+            }
+        }
+        if (!includes.isEmpty()) {
+            final PlexusIoFileResourceCollection collection = new PlexusIoFileResourceCollection();
+            if (sourceDirectory != null) {
+                collection.setBaseDir(sourceDirectory);
+            }
+            final FileSelector[] selectors;
+            final IncludeExcludeFileSelector fs = new IncludeExcludeFileSelector();
+            final String[] inc = new String[includes.size()];
+            fs.setIncludes(includes.toArray(inc));
+            selectors = new FileSelector[]{fs};
+            collection.setFileSelectors(selectors);
+            final Iterator<PlexusIoResource> resources;
+            try {
+                resources = collection.getResources();
+            } catch (IOException e) {
+                throw new MojoFailureException("Failed to get list of XAR XML files", e);
+            }
+            while (resources.hasNext()) {
+                PlexusIoResource resource = resources.next();
+                if (resource.isFile()) {
+                    configurationArtifactFiles.add(new File(collection.getBaseDir(), resource.getName()));
+                }
+            }
         }
 
-        try {
-            final JAXBContext jc = JAXBContext.newInstance(ConfigurationType.class.getPackage().getName(), getClass().getClassLoader());
-            final Unmarshaller unmarshaller = jc.createUnmarshaller();
-            final ConfigurationType configuration = ((JAXBElement<ConfigurationType>) unmarshaller.unmarshal(configurationArtifactFile)).getValue();
+        final Set<String> completed = new TreeSet<>();
+        for (final File configurationArtifactFile : configurationArtifactFiles) {
+            log.info("Executing configuration: " + configurationArtifactFile);
 
-            List modelContexts = Lists.newArrayList();
-
-            if (configuration.getModels() != null) {
-                modelContexts.addAll(configuration.getModels().getModel().stream().map(m -> new Model(m).toModelContext()).collect(Collectors.toList()));
-            }
-            if (configuration.getXmlModels() != null) {
-                modelContexts.addAll(configuration.getXmlModels().getXmlModel().stream().map(m -> new XmlModel(m).toModelContext()).collect(Collectors.toList()));
-            }
-            if (configuration.getPlainXmlModels() != null) {
-                modelContexts.addAll(configuration.getPlainXmlModels().getPlainXmlModel().stream().map(m -> new PlainXmlModel(m).toModelContext()).collect(Collectors.toList()));
-            }
-            if (configuration.getExcelModels() != null) {
-                modelContexts.addAll(configuration.getExcelModels().getExcelModel().stream().map(m -> new ExcelModel(m).toModelContext()).collect(Collectors.toList()));
+            if (!configurationArtifactFile.exists()) {
+                throw new MojoFailureException("Configuration artifact file not found: " + configurationArtifactFile);
             }
 
-            try (ExecutionContext executionContext = ExecutionContext.builder()
-                    .metaModels(configuration.getMetaModels().getMetaModel())
-                    .modelContexts(modelContexts)
-                    .artifactResolver(MavenArtifactResolver.builder()
-                            .repoSession(repoSession)
-                            .repositories(repositories)
-                            .repoSystem(repoSystem)
-                            .log(log)
-                            .build())
-                    .profile(configuration.isProfile() != null ? configuration.isProfile() : false)
-                    .sourceDirectory(sourceDirectory)
-                    .log(log)
-                    .build()) {
+            try {
+                final JAXBContext jc = JAXBContext.newInstance(ConfigurationType.class.getPackage().getName(), getClass().getClassLoader());
+                final Unmarshaller unmarshaller = jc.createUnmarshaller();
+                final ConfigurationType configuration = ((JAXBElement<ConfigurationType>) unmarshaller.unmarshal(configurationArtifactFile)).getValue();
 
-                executionContext.init();
-                configuration.getEolPrograms().getEclOrEglOrEgx().forEach(prg -> {
-                    final EolExecutionContext eolExecutionContext;
-                    if (prg instanceof EclType) {
-                        eolExecutionContext = Ecl.builder().ecl((EclType) prg).build().toExecutionContext();
-                    } else if (prg instanceof EglType) {
-                        eolExecutionContext = Egl.builder().egl((EglType) prg).build().toExecutionContext();
-                    } else if (prg instanceof EgxType) {
-                        eolExecutionContext = Egx.builder().egx((EgxType) prg).build().toExecutionContext();
-                    } else if (prg instanceof EmlType) {
-                        eolExecutionContext = Eml.builder().eml((EmlType) prg).build().toExecutionContext();
-                    } else if (prg instanceof EtlType) {
-                        eolExecutionContext = Etl.builder().etl((EtlType) prg).build().toExecutionContext();
-                    } else if (prg instanceof EvlType) {
-                        eolExecutionContext = Evl.builder().evl((EvlType) prg).build().toExecutionContext();
-                    } else if (prg instanceof EolType) {
-                        eolExecutionContext = Eol.builder().eol((EolType) prg).build().toExecutionContext();
-                    } else {
-                        log.error("Unsupported Epsilon program type: " + prg);
-                        eolExecutionContext = null;
-                    }
-                    if (eolExecutionContext != null) {
+                List modelContexts = Lists.newArrayList();
+
+                if (configuration.getModels() != null) {
+                    modelContexts.addAll(configuration.getModels().getModel().stream().map(m -> new Model(m).toModelContext()).collect(Collectors.toList()));
+                }
+                if (configuration.getXmlModels() != null) {
+                    modelContexts.addAll(configuration.getXmlModels().getXmlModel().stream().map(m -> new XmlModel(m).toModelContext()).collect(Collectors.toList()));
+                }
+                if (configuration.getPlainXmlModels() != null) {
+                    modelContexts.addAll(configuration.getPlainXmlModels().getPlainXmlModel().stream().map(m -> new PlainXmlModel(m).toModelContext()).collect(Collectors.toList()));
+                }
+                if (configuration.getExcelModels() != null) {
+                    modelContexts.addAll(configuration.getExcelModels().getExcelModel().stream().map(m -> new ExcelModel(m).toModelContext()).collect(Collectors.toList()));
+                }
+
+                try (ExecutionContext executionContext = ExecutionContext.builder()
+                        .metaModels(configuration.getMetaModels().getMetaModel())
+                        .modelContexts(modelContexts)
+                        .artifactResolver(MavenArtifactResolver.builder()
+                                .repoSession(repoSession)
+                                .repositories(repositories)
+                                .repoSystem(repoSystem)
+                                .log(log)
+                                .build())
+                        .profile(configuration.isProfile() != null ? configuration.isProfile() : false)
+                        .sourceDirectory(sourceDirectory)
+                        .log(log)
+                        .build()) {
+
+                    executionContext.init();
+                    configuration.getEolPrograms().getEclOrEglOrEgx().forEach(prg -> {
+                        final EolExecutionContext eolExecutionContext;
+                        if (prg instanceof EclType) {
+                            eolExecutionContext = Ecl.builder().ecl((EclType) prg).build().toExecutionContext();
+                        } else if (prg instanceof EglType) {
+                            eolExecutionContext = Egl.builder().egl((EglType) prg).build().toExecutionContext();
+                        } else if (prg instanceof EgxType) {
+                            eolExecutionContext = Egx.builder().egx((EgxType) prg).build().toExecutionContext();
+                        } else if (prg instanceof EmlType) {
+                            eolExecutionContext = Eml.builder().eml((EmlType) prg).build().toExecutionContext();
+                        } else if (prg instanceof EtlType) {
+                            eolExecutionContext = Etl.builder().etl((EtlType) prg).build().toExecutionContext();
+                        } else if (prg instanceof EvlType) {
+                            eolExecutionContext = Evl.builder().evl((EvlType) prg).build().toExecutionContext();
+                        } else {
+                            eolExecutionContext = Eol.builder().eol(prg).build().toExecutionContext();
+                        }
                         executionContext.executeProgram(eolExecutionContext);
-                    }
-                });
-                executionContext.commit();
-            } catch (Exception e) {
-                throw new MojoExecutionException("Execution error: " + e.toString(), e);
+                    });
+                    executionContext.commit();
+                    completed.add(configurationArtifactFile.getAbsolutePath());
+                }
+            } catch (Exception ex) {
+                log.debug("Completed configuration artifacts: \n" + String.join(",\n", completed));
+                throw new MojoExecutionException("Execution error: " + ex.getMessage(), ex);
             }
-        } catch (JAXBException ex) {
-            throw new MojoExecutionException("Execution error: " + ex.getMessage(), ex);
         }
+        log.info("Completed configuration artifacts: \n" + String.join(",\n", completed));
     }
 }
