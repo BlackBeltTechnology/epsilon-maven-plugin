812a9654c41405c706401ce8b53e4680760a587b
Robert Csakany <robert.csakany@blackbelt.hu>
2019-01-17 12:56:50 +0100

    #7 Add export trace model in ETL

--- src/main/java/hu/blackbelt/epsilon/maven/plugin/execute/Etl.java
+++ src/main/java/hu/blackbelt/epsilon/maven/plugin/execute/Etl.java
@@ -3,10 +3,15 @@ package hu.blackbelt.epsilon.maven.plugin.execute;
 import hu.blackbelt.epsilon.runtime.execution.contexts.EolExecutionContext;
 import hu.blackbelt.epsilon.runtime.execution.contexts.EtlExecutionContext;
 import hu.blackbelt.epsilon.runtime.execution.contexts.ProgramParameter;
+import org.apache.maven.plugins.annotations.Parameter;
 
 import java.util.stream.Collectors;
 
 public class Etl extends Eol {
+
+    @Parameter(name = "exportTransformationState", readonly = false, required = true)
+    String exportTransformationState;
+
 
     @Override
     EolExecutionContext toExecutionContext() {
@@ -14,6 +19,7 @@ public class Etl extends Eol {
                 .parameters(parameters.stream()
                         .map(p -> ProgramParameter.builder().name(p.name).value(p.value).build())
                         .collect(Collectors.toList()))
+                .exportTransformationTrace(exportTransformationState)
                 .source(source)
                 .build();
     }
